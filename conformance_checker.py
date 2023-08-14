47b654a73675190cb418e74b04c150beadfdfdfb
lakhoune <a.b.lakhoune@gmail.com>
2023-08-14 21:42:50 +0200

    add function to fetch event log

--- conformance_checker.py
+++ conformance_checker.py
@@ -3,6 +3,7 @@ import json
 import pm4py
 import os
 import requests
+import xml.etree.ElementTree as ET
 
 from db_utils import read_events_into_df, get_db_connection
 
@@ -25,6 +26,7 @@ db_connection_bots = get_db_connection(mysql_host,mysql_port, mysql_user, mysql_
 
 bot_model_file_path = "./models/bot_model.json"
 event_log_file_path = "event_logs/test_event_log.xes"
+tmp_event_log_file_path = "event_logs/tmp_event_log.xes"
 
 
 def load_default_bot_model():
@@ -48,6 +50,23 @@ def fetch_bot_model(name):
     if request.status_code == 200:
         return request.json()
     else:
+        return None
+
+def fetch_event_log(resource_id):
+    # fetches an event log from the social bot manager. available at <base_url>/event_logs/{name}
+    response = requests.get(f"https://mobsos.tech4comp.dbis.rwth-aachen.de/event-log/resource/{resource_id}")
+    # response is xml, use pm4py to parse it
+    if response.status_code == 200:
+        xml = response.content
+        root = ET.fromstring(xml)
+        # write the xml to a file 
+        with open(tmp_event_log_file_path, "wb") as f:
+            f.write(xml)
+        
+        # read the file with pm4py and return the event log and delete the file
+        return pm4py.read_xes(tmp_event_log_file_path), os.remove(tmp_event_log_file_path)
+    else:
+        print(response.status_code)
         return None
 
 def get_bot_agentids(name):
