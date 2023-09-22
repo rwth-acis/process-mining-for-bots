dd70a5bbce7cc49ed9ac27f6db3c974894e4507a
lakhoune <a.b.lakhoune@gmail.com>
2023-09-22 11:18:59 +0200

    rename file, add mobsos requests

--- utils/api_requests.py
+++ utils/api_requests.py
@@ -1,10 +1,10 @@
-import requests
-import xml.etree.ElementTree as ET
+import requests as r
 import pm4py
-import os
 import pandas as pd
 import json
 from pm4py.objects.log.importer.xes import variants as xes_importer
+import base64
+
 
 bot_model_file_path = "./assets/models/test_bot_model.json"
 event_log_file_path = "assets/event_logs/test_event_log.xes"
@@ -12,7 +12,7 @@ event_log_file_path = "assets/event_logs/test_event_log.xes"
 def fetch_bot_model(name , endpoint = "https://mobsos.tech4comp.dbis.rwth-aachen.de/SBFManager"):
     # fetches a bot model from the social bot manager. available at <base_url>/models/{name}
     print(f"Fetching bot model from {endpoint}/models/{name}")
-    request = requests.get(f"{endpoint}/models/{name}")
+    request = r.get(f"{endpoint}/models/{name}")
     if request.status_code == 200:
         return request.json()
     else:
@@ -23,7 +23,7 @@ def fetch_event_log(bot_name, url=None):
         url = f"https://mobsos.tech4comp.dbis.rwth-aachen.de/event-log"
     # fetches an event log from the social bot manager. available at <base_url>/event_logs/{name}
     print(f"Fetching event log from {url}/bot/{bot_name}")
-    response = requests.get(f"{url}/bot/{bot_name}")
+    response = r.get(f"{url}/bot/{bot_name}")
     # response is xml, use pm4py to parse it
     if response.status_code == 200:
         xml = response.content
@@ -68,3 +68,42 @@ def load_bot_model(path=None):
             #load the json file
             bot_model = json.load(json_file)
             return bot_model
+        
+
+def fetch_success_model(endpoint, botName,bot_pw,group_id=None,service_id=None):
+    if group_id is None:
+        group_id = "343da947a6db1296fadb5eca3987bf71f2e36a6d088e224a006f4e20e6e7935bb0d5ce0c13ada9966228f86ea7cc2cf3a1435827a48329f46b0e3963213123e0"
+    if service_id is None:
+        service_id = "i5.las2peer.services.mensaService.MensaService"
+    endpoint += group_id +"/"+ service_id
+    headers = {'authorization': __getAuthorizationHeader(botName,bot_pw)}
+    print(f"Fetching success model from {endpoint}")
+    success_model_response = r.get(f"{endpoint}",headers=headers)
+    return success_model_response.json()["xml"] if success_model_response.status_code == 200 else None
+
+
+def __getAuthorizationHeader(user_name, pw):
+    """
+    Gets the authorization header for basic auth using useranme and password
+    :param user_name: the username  
+    :param pw: the password
+    :return: the authorization header
+    """
+    return f"""Basic {base64.b64encode(f"{user_name}:{pw}".encode('utf-8')).decode('utf-8')}"""
+
+def fetchL2PGroups(success_modeling_service_endpoint,user_name,pw):
+    """
+    Fetches all Las2peer groups of a user
+    """
+    success_modeling_service_endpoint += "/groups"
+    response = r.get(success_modeling_service_endpoint,headers={'authorization': __getAuthorizationHeader(user_name,pw),
+                                       'Content-Type': 'application/json'})
+    return response.json() if response.status_code == 200 else None
+
+def fetchL2PServices(endpoint = "https://mobsos.tech4comp.dbis.rwth-aachen.de/mobsos-success-modeling/apiv2/services"):
+    """
+    Fetches all Las2peer services available at the given endpoint
+    """
+    print(f"Fetching services from {endpoint}")
+    response = r.get(endpoint)
+    return response.json() if response.status_code == 200 else None
