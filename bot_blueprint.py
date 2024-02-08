157831554b5db8756f4315ce75ac1868999f15f8
lakhoune <a.b.lakhoune@gmail.com>
2024-02-08 12:05:30 +0100

    fix return value

--- bot_blueprint.py
+++ bot_blueprint.py
@@ -67,7 +67,7 @@ def enhanced_bot_model(botName):
 
     try:
         bot_parser = get_parser(bot_model_json)
-        bot_model_dfg, start_activities, end_activities, performance_dfg, frequency_dfg = enhance_bot_model(
+        bot_model_dfg, start_activities, end_activities, frequency_dfg, performance_dfg = enhance_bot_model(
             event_log, bot_parser,repair=True)
         if res_format == 'svg':
             gviz = dfg_visualizer.apply(bot_model_dfg)
